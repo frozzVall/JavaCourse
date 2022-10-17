3851605d67bd59f7e198a58655bd3da4437edf5c
frozzVall <lera70939@gmail.com>
2022-10-17 22:02:11 +0300

    added several features

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,10 +1,25 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram
-{
+public class MyFirstProgram {
 
-  public static void main(String[] args)
-  {
-    System.out.println("Hello,world!");
+  public static void main(String[] args) {
+    hello("Lera");
+    hello("Evgeniy");
+    double len = 5;
+    System.out.println("square area with side " + len + "=" + area(len));
+    double side1=4;
+    double side2=6;
+    System.out.println("area of a rectangle with sides  " + side1 +" and " + side2 + "=" + area(side1,side2));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double I) {
+    return I * I;
+  }
+  public static double area(double a, double b){
+    return a*b;
   }
 }
