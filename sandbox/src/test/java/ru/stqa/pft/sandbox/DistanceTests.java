62038c84d55112124477814fff0fd5d719cc8b93
frozzVall <lera70939@gmail.com>
2022-10-24 14:30:40 +0300

    Distance Tests are changed

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
@@ -5,18 +5,18 @@ import org.testng.annotations.Test;
 
 public class DistanceTests {
   @Test
-  public void testPoint(){
-    Point p1=new Point(-1,3);
-    Point p2=new Point(6,2);
-    double delta=0.001;
-    Assert.assertEquals(p2.distance(p1),7.071,delta);
+  public void testPoint() {
+    Point p1 = new Point(-1, 3);
+    Point p2 = new Point(6, 2);
+    double delta = 0.001;
+    Assert.assertEquals(p2.distance(p1), 7.071, delta);
   }
+
   @Test
-  public void testPoint1(){
-    Point p1=new Point(-1,3);
-    Point p2=new Point(6,2);
-    double delta=0.001;
-    Assert.assertEquals(p2.distance(p1),-7.071,delta);
+  public void testPoint1() {
+    Point p1 = new Point(-1, 3);
+    Point p2 = new Point(6, 2);
+    double delta = 0.001;
+    Assert.assertEquals(p2.distance(p1), 7.0710678118654755, delta);
   }
 }
-
