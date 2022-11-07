42bc28bce258538abbec531afd0298a0177dd58d
frozzVall <lera70939@gmail.com>
2022-11-07 15:14:14 +0300

    enforcement of preconditions3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/СontactsModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/СontactsModificationTests.java
@@ -11,7 +11,7 @@ public class СontactsModificationTests extends TestBase {
     }
     app.getNavigationHelper().returnToHomePage();
     app.getContactHelper().initContactsModification();
-    app.getContactHelper().fillContacts(new Contacts("alina", "dlogyv", "uliza gorelika", "+375291567859", "liza709@gmail.com",null),false);
+    app.getContactHelper().fillContacts(new Contacts("alina", null, null, null, null,null),false);
     app.getContactHelper().submitContactsModification();
     app.getNavigationHelper().returnToHomePage();
   }
